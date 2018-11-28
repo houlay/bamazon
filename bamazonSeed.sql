ba127d58c4614818e7dc5468a63f2c6bc44f2b02
Chad Pan <cppan0@gmail.com>
2018-11-28 16:04:08 -0600

    completed schema file

---
+++ bamazonSeed.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INTEGER(100) NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(20) NOT NULL,
+    price DECIMAL(100,2) NOT NULL,
+    stock_quantity INTEGER(200) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Nerf Gun", "Toys & Games", 12.99, 50), ("Nintendo Switch", "Toys & Games", 299.99, 10), ("Roblox", "Toys & Games", 14.99, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Headset", "Musical Instruments", 11.78, 20), ("Microphone", "Musical Instruments", 7.58, 30), ("Acoustic Guitar", "Musical Instruments", 32.47, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Paper Towel", "Grocery", 30.44, 25), ("Coconut Oil", "Grocery", 10.58, 50), ("Bottle of Water", "Grocery", 0.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUE ("Starwars", "Books", 12.50, 10);
